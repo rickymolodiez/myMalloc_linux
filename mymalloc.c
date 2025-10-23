71f1fd878814132cd75dfec10ef6652ec6cf46e5
rickymolodiez <rickymoloqc@gmail.com>
2025-10-23 00:11:44 -0400

    fix function, make it work, print address.

--- mymalloc.c
+++ mymalloc.c
@@ -2,53 +2,34 @@
 #include <sys/syscall.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/mman.h>
 
-#define SYS_mmap 197
 
 void *mallocAndPrint(size_t myLength, int prot, int flags, int fileDescriptor, int myOffset, char *message);
-// https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man2/mmap.2.html
-//197	AUE_MMAP	ALL	{ user_addr_t mmap(caddr_ut addr, size_ut len, int prot, int flags, int fd, off_t pos) NO_SYSCALL_STUB; }
-//from xnu repo
-/*args for 197 :
-ALL = architectures supported.
-*/
-//compilemsg
-// gcc -Wno-deprecated-declarations mymalloc.c -o malloc
-//
+
 
 int main(){
     char content[100];
     printf("Enter something meaningful (or not) to be stored in memory: ");
     scanf("%s", content);   
     int sizeM;
-    printf("Enter the length (in bytes) of the desired memory partition:\n");
+    printf("Enter the length (in kilobytes) of the desired memory partition:\n");
     scanf("%d", &sizeM);
     size_t size =  (size_t) sizeM + strlen(content);
     int prot = 0x1 | 0x2;
     int flags = -1;
     int fd = -1;
     int offset = 0;
-    mallocAndPrint(size, prot, flags, fd, offset, content);
-
-return 0;
-
-}
-
-
-void *mallocAndPrint(size_t myLength, int prot, int flags, int fileDescriptor, int myOffset, char *message){
-
-long result = syscall(197,NULL, myLength ,prot, flags, fileDescriptor, myOffset); //used syscal 197 to allocate enough memory for string inputted by user
-void *ptr = (void *) result;
-if (ptr == (void *)-1) {
-        perror("syscall mmap failed");
-        return NULL;
+    long len=size*1024;
+    void *addr = mmap(NULL, len, prot, flags, fd, offset);
+    if(addr==0){
+        perror("system call mmap has failed!");
     }
-const char hMessage[20] = "Hello from Memory!\n";
-
+    int *vmAddr = (int*)addr;
+    printf("if you didn't see the error message, then that means mmap has allocated virtual memoryat %p\n", vmAddr); 
 
 return 0;
 
 }
-
 
 
